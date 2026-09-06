24d9fd30f3bb4e5f05ca5617d3b887fbb14ef3c2
mahvash <mohammadmahvash1@gmail.com>
2026-09-06 18:12:58 +0330

    status code

--- accounts/api_views.py
+++ accounts/api_views.py
@@ -1,12 +1,15 @@
 from rest_framework.views import APIView
-from .serializers import UserSerializer , UserRegisterSerializer
+from .serializers import UserSerializer, UserRegisterSerializer
 from rest_framework.response import Response
+from rest_framework import status
 from .models import User
+
+
 class UserRegisterView(APIView):
     def post(self, request):
         serializer = UserRegisterSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = User(email=serializer.validated_data['email'],username=serializer.validated_data['username'])
+        user = User(email=serializer.validated_data['email'], username=serializer.validated_data['username'])
         user.set_password(serializer.validated_data['password'])
         user.save()
-        return Response(UserSerializer(user).data)
+        return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
--- questions/api_views.py
+++ questions/api_views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Question
 from .serializers import QuestionsListSerializer, QuestionDetailSerializer
@@ -9,11 +10,11 @@ class QuestionsListView(APIView):
     def get(self, request):
         questions = Question.objects.all()
         serialized_data = QuestionsListSerializer(instance=questions, many=True)
-        return Response(serialized_data.data)
+        return Response(serialized_data.data, status=status.HTTP_200_OK)
 
 
 class QuestionDetailView(APIView):
     def get(self, request, pk):
         questions = Question.objects.filter(id=pk).first()
         serialized_data = QuestionDetailSerializer(instance=questions)
-        return Response(serialized_data.data)
+        return Response(serialized_data.data, status=status.HTTP_200_OK)
